3218268835d554ea81399161600fe07e0133060d
zion <gomjunho@gmail.com>
2026-08-13 09:51:12 +0900

    📊 style: rebrand real estate insight navbar menu into 📊 자산 증식 인사이트

--- web/test/controllers/insights_controller_test.rb
+++ web/test/controllers/insights_controller_test.rb
@@ -11,7 +11,7 @@ class InsightsControllerTest < ActionDispatch::IntegrationTest
   test "should get show view with scatter plot, urgent deals, and wealth investment proposals" do
     get insight_path
     assert_response :success
-    assert_select "h1", text: /부 증식 부동산 인사이트/
+    assert_select "h1", text: /자산 증식 인사이트/
     assert_select "h2", text: /급매물 & 시세 이하/
     assert_select "h2", text: /부동산 트렌드 & 하이엔드 절세/
     assert_select "h2", text: /VVIP 자산 증식 & 부동산 프라이빗 투자 제안/
@@ -21,7 +21,7 @@ class InsightsControllerTest < ActionDispatch::IntegrationTest
     RealEstateTransaction.destroy_all
     get insight_path
     assert_response :success
-    assert_select "h1", text: /부 증식 부동산 인사이트/
+    assert_select "h1", text: /자산 증식 인사이트/
   end
 
   test "should return api transactions and wealth expansion intelligence" do
